fefc13aa3d11af1465a282e229cde2f058ee1fe8
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-03-12 00:39:40 +0900

    implemented describe function
    This function describes a character who calls it by using gpt-3.5.

--- Character.py
+++ Character.py
@@ -1,11 +1,37 @@
+import os
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 class Character:
-    def __init__(self):
-        self.name = ""
-        self.id = ""
-        self.relationship = ""
-        self.items = ""
-        self.background = ""
-        self.personality = ""
-        self.race = ""
+    def __init__(self, name, id, relationships, items, background, personality, race):
+        self.name = name
+        self.id = id
+        self.relationships = relationships
+        self.items = items
+        self.background = background
+        self.personality = personality
+        self.race = race
+        self.description = None
+        self.changed = False
     def describe(self):
-        pass
+        if (not self.changed) and (self.description is not None):
+            return self.description
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Summarize information about me."
+                                            "\nname:{}"
+                                            "\nrelationships:{}"
+                                            "\nitems:{}"
+                                            "\nbackground:{}"
+                                            "\npersonality:{}"
+                                            "\nrace:{}".format(self.name, self.relationships,
+                                                               self.items, self.background,
+                                                               self.personality, self.race)
+                }
+            ]
+        )
+        # print(response["choices"][0]["message"]["content"])
+        return response["choices"][0]["message"]["content"]

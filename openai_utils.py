9b47a9b37373fa09c9b50a1dc41feac88a974096
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-04-02 21:58:43 +0900

    Create openai_utils.py
    Implemented an easy to use interface.

---
+++ openai_utils.py
@@ -0,0 +1,19 @@
+import openai
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def get_answer(self, system_prompt, user_prompt, return_token=False):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user",
+             "content": user_prompt}
+        ]
+    )
+    answer = response["choices"][0]["message"]["content"]
+    if return_token:
+        token_used = response['usage']['total_tokens']
+        return answer, token_used
+    return answer

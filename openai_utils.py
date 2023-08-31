63028ef175775d074017ea3f7789228afde083e0
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-09-01 02:27:44 +0900

    Create create_prompt function
    Create create_prompt function

--- openai_utils.py
+++ openai_utils.py
@@ -3,7 +3,6 @@ import json
 import os
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 def get_answer(user_prompt, system_prompt=None, return_token=False):
     messages = []
     if system_prompt is not None:
@@ -20,4 +19,42 @@ def get_answer(user_prompt, system_prompt=None, return_token=False):
     return answer
 
 
-
+def create_prompt(universe="cyberpunk"):
+    prompt = """Create an image that depicts this: 
+    "{}"
+    """.format(universe)
+    messages = [{"role": "user", "content": prompt}]
+    functions = [
+        {
+            "name": "draw_image",
+            "description": """Text-to-image generation model
+The prompt consists of keywords, which are descriptive adjectives or nouns to add depth and flavor.
+The negative_prompt are the descriptive adjectives or keywords that you don't want included in the image.
+For example, if the prompt is "cat swimming in day time", you could add "day" as a Keyword and "night" or "dark" as a negative_prompt.
+Please follow this exact pattern and do not make up your own.
+            """,
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "prompt": {
+                        "type": "string",
+                        "description": "The prompt to guide the image generation",
+                    },
+                    "negative_prompt": {
+                        "type": "string",
+                        "description": "The prompt not to guide the image generation",
+                    },
+                },
+                "required": ["prompt", "negative_prompt"],
+            },
+        }
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        functions=functions,
+        function_call="auto",  # auto is default, but we'll be explicit
+    )
+    response_message = response["choices"][0]["message"]
+    function_args = json.loads(response_message["function_call"]["arguments"])
+    return function_args

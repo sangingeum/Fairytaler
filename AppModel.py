916412a48afc8f2ed39e63108d9538a9e3cb0c07
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-09-08 03:24:16 +0900

    Edit save & load, change interface
    Edit save & load, change interface

--- AppModel.py
+++ AppModel.py
@@ -2,6 +2,7 @@ from OpenAIUtils import *
 from Character import *
 from ImageCreator import *
 import threading
+from concurrent.futures import ThreadPoolExecutor
 import pickle
 
 # TODOS:
@@ -9,13 +10,19 @@ import pickle
 # 게임 저장, 로드, 종료
 # 게임 중간에 아이템 지급, 제거 이벤트
 # 16k 컨텍스트를 넘어서도 내용이 이어지게
+# 유저 인풋을 기다릴 때만 저장이 가능하게 하자
 
 class AppModel:
     def __init__(self):
-        self.image_creator = ImageCreator()
         self.protagonist = None
         self.universe = None
         self.messages = []
+        self.main_text = ""
+        self.waiting_user_input = False
+        self.image_count = 0
+        self.image_creator = ImageCreator()
+        self.image_list_lock = threading.Lock()
+        self.images = []
         # prompts
         self.fictional_universe_expander = """I want you to act as a novel writer.
 The fictional universe of the story is like this : {}
@@ -48,144 +55,97 @@ The universe the player is in is like this:
 The universe the player is in is like this: 
 {2}
 """
+        self.new_game_text = """{}
+        
+[System] You received a(an) {}, {}. It can be equipped on your {}.
 
-    # basic operations
-    def save(self, path=None):
-        if path is None:
-            path = "saves/save0.pkl"
-        print("Saving [" + path + "]")
-        with open(path, 'wb') as file:
-            pickle.dump(self.messages, file)
-            pickle.dump(self.protagonist, file)
-            pickle.dump(self.universe, file)
-        print("Saving Complete")
+{}
+"""
 
-    def load(self, path=None):
-        if path is None:
-            path = "saves/save0.pkl"
-        print("Loading [" + path + "]")
-        with open(path, 'wb') as file:
-            self.messages = pickle.load(file)
-            self.protagonist = pickle.load(file)
-            self.universe = pickle.load(file)
-        print("Loading Complete")
-    def start(self):
-        self.tell_story()
-
-    def exit(self):
-        exit()
-
-    def new_game(self):
-        self.init()
-        self.start()
-
-    def load_latest(self):
-        self.load()
-
-    def lobby_menu(self):
-        print("What would you like to do?")
-        print("1. new game")
-        print("2. load")
-        while True:
-            user_input = input("Enter number(1~3):")
-            if user_input in ["1", "2", "3"]:
-                if user_input == "1":
-                    self.new_game()
-                    break
-                elif user_input == "2":
-                    self.load()
-                elif user_input == "3":
-                    self.exit()
-
-
-
-    def in_game_menu(self):
-        print("What would you like to do?")
-        print("1. save")
-        print("2. load")
-        print("3. exit")
-        print("4. go to lobby")
-        print("5. go back to game")
-        while True:
-            user_input = input("Enter number(1~5):")
-            if user_input in ["1", "2", "3", "4", "5"]:
-                if user_input == "1":
-                    self.save()
-                elif user_input == "2":
-                    self.load()
-                elif user_input == "3":
-                    self.exit()
-                elif user_input == "4":
-                    self.lobby_menu()
-                elif user_input == "5":
-                    pass
-                break
-
-    def ask_and_confirm(self, question):
-        while True:
-            user_input = input(question + "\n")
-            yes_or_no = input("Do you want to change your answer? (y/n):")
-            if yes_or_no.lower() == "y":
-                continue
-            break
-        return user_input
-
-    def init(self):
-        # Set the universe
-        universe = self.ask_and_confirm("Describe the fictional universe you want to explore.")
+    def new_game(self, universe, name, gender, race, personality, background):
+        print("new game")
         self.universe = universe
-        # Set the protagonist name
-        name = self.ask_and_confirm("Write the name of the protagonist.")
-        # Set the protagonist gender
-        gender = self.ask_and_confirm("Write the gender of the protagonist. (male/female)")
-        # Set the protagonist race
-        race = self.ask_and_confirm("What is the race of the protagonist?")
-        # Set the protagonist personality
-        personality = self.ask_and_confirm("Describe the personality of the protagonist.")
-        # Set the protagonist background
-        background = self.ask_and_confirm("Describe the background of the protagonist.")
-
-        # Create an equipable item
-        print("We will give you an equipment as a gift.")
-        item = create_equipable_item(self.equipment_generation_command.format(race, background, universe))
-        print("You received a(an) {}, {}, which can be put on your {}.".format(item["name"], item["description"], item["slot"]))
-
-        # Create the protagonist
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            item_future = executor.submit(create_equipable_item, self.equipment_generation_command.format(race, background, universe))
+            universe_future = executor.submit(get_answer, self.fictional_universe_expander.format(self.universe))
+        item = item_future.result()
+        # create the protagonist
         character = Character(name=name, id=0, relationships=[], companions=[], consumables=[], equipments=[],
-                    equipments_in_use={item["slot"]: item}, background=background, personality=personality, race=race,
-                    gender=gender)
+                              equipments_in_use={item["slot"]: item}, background=background, personality=personality,
+                              race=race, gender=gender)
         self.protagonist = character
-
-        # Replace the existing universe
-        prompt = self.fictional_universe_expander.format(self.universe)
-        self.universe = get_answer(prompt)
-        # print the universe description
-        print(self.universe)
-        # Create initial image
-        image_prompt = create_prompt(self.image_prompt_generator.format(self.universe))
-
-        image_thread = threading.Thread(target=self.image_creator.create, args=(image_prompt["prompt"], image_prompt["negative_prompt"],))
-        image_thread.start()
-        #self.image_creator.create(image_prompt["prompt"], image_prompt["negative_prompt"])
-
-    def tell_story(self):
+        self.universe = universe_future.result()
+        # init message and get answer
         user_prompt = self.game_start_command.format(self.protagonist.name, self.universe, self.protagonist.describe())
         self.messages = []
-        while True:
+        self.messages.append({"role": "user", "content": user_prompt})
+        assistant_answer = chat_completion(self.messages)
+        self.messages.append({"role": "assistant", "content": assistant_answer})
+
+        self.waiting_user_input = True
+
+        main_text = self.new_game_text.format(self.universe, item["name"], item["description"], item["slot"], assistant_answer)
+        context_1 = self.image_prompt_generator.format(self.universe)
+        context_2 = self.image_prompt_generator.format(assistant_answer)
+        self.main_text = main_text
+        return main_text, context_1, context_2
+
+    def process_user_text(self, user_prompt):
+        if self.waiting_user_input:
+            self.main_text += "\n" + user_prompt + "\n"
+            self.waiting_user_input = False
             self.messages.append({"role": "user", "content": user_prompt})
             assistant_answer = chat_completion(self.messages)
             self.messages.append({"role": "assistant", "content": assistant_answer})
-            #print(user_prompt)
-            print(assistant_answer)
-            # Create image
-            image_prompt = create_prompt(self.image_prompt_generator.format(assistant_answer))
-            image_thread = threading.Thread(target=self.image_creator.create, args=(image_prompt["prompt"], image_prompt["negative_prompt"],))
+            self.main_text += "\n" + assistant_answer + "\n"
+            image_thread = threading.Thread(target=self.create_image_and_append, args=(assistant_answer,))
             image_thread.start()
-            #self.image_creator.create(image_prompt["prompt"], image_prompt["negative_prompt"])
-            user_prompt = input(":")
-            if user_prompt == "menu":
-                self.main_menu()
+            self.waiting_user_input = True
+            return assistant_answer
+        return None
 
+    def create_image_and_append(self, context):
+        image_prompt = create_prompt(self.image_prompt_generator.format(context))
+        image = self.image_creator.create(image_prompt["prompt"], image_prompt["negative_prompt"])
+        with self.image_list_lock:
+            self.images.append(image)
+        return image
 
+    def save(self, path):
+        try:
+            if self.waiting_user_input:
+                print("Saving [" + path + "]")
+                with open(path, 'wb') as file:
+                    pickle.dump(self.protagonist, file)
+                    pickle.dump(self.universe, file)
+                    pickle.dump(self.messages, file)
+                    pickle.dump(self.main_text, file)
+                    pickle.dump(self.waiting_user_input, file)
+                    pickle.dump(self.image_count, file)
+                    pickle.dump(self.images, file)
+                print("Saving Complete")
+                return True
+            return False
+        except:
+            return False
 
+    def load(self, path):
+        try:
+            print("Loading [" + path + "]")
+            with open(path, 'rb') as file:
+                self.protagonist = pickle.load(file)
+                self.universe = pickle.load(file)
+                self.messages = pickle.load(file)
+                self.main_text = pickle.load(file)
+                self.waiting_user_input = pickle.load(file)
+                self.image_count = pickle.load(file)
+                self.images = pickle.load(file)
+            self.image_creator.change_image_count(self.image_count)
+            print("Loading Complete")
+            return True
+        except:
+            return False
 
+    def exit(self):
+        print("exit")
+        exit()
